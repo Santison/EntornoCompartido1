313ec8ea9482d93a4f2258178a49fdfe4f33754a
IES Delgado Hndz <IES Delgado Hndz@DESKTOP-LSEV2JM>
2024-03-11 21:00:35 +0100

    version inicial del proyecto

---
+++ Proyecto1/src/Paquete1/java/Principal.java
@@ -0,0 +1,11 @@
+package Paquete1.java;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Estamos haciendo cosas con git");
+		
+	}
+
+}
