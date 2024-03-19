14760b17942668445391086129dc062f797361bf
IES Delgado Hndz <IES Delgado Hndz@DESKTOP-LSEV2JM>
2024-03-19 15:59:14 +0100

    aaaaa

--- EjercicioParejas/src/Semaforo/Semaforo1.java
+++ EjercicioParejas/src/Semaforo/Semaforo1.java
@@ -4,7 +4,7 @@ public class Semaforo1 {
     private String estado;
 
     public Semaforo1() {
-        this.estado = "rojo"; // El semáforo comienza en estado rojo por defecto aa
+        this.estado = "rojo"; // El semáforo comienza en estado rojo por defecto aaaa
     }
 
     public void cambiarEstado() {
